a6fc095a25aa5ea64cfef3bc66c3263bf9a7f249
v4464 <v4464781@gmail.com>
2024-04-18 13:37:47 +0530

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path'); 
+const express = require('express');
+const router = express.Router();
+
+const rootdir=require('../util/path')
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootdir, 'views', 'add-product.html'));
+});
+
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const path=require('path');
+const express=require('express');
+const router=express.Router();
+
+const rootdir=require('../util/path')
+
+
+router.get('/',(req,res,next) => {
+    res.sendFile(path.join(rootdir,'views','shop.html'));
+})
+module.exports=router;
